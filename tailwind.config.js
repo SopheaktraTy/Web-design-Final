60fbf7ba986b4d7e8a9ae07f8f213712cdf3f0c0
TY SOPHEAKTRA <pheaktra.st67@gmail.com>
2023-07-25 00:30:14 +0700

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor: '#3DCAB1',
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/asset/image/6bb8d9059775fa57f5_Mount_Everest_as_seen_from_Drukair2.jpg')",
+        
+      }
+    },
   },
   plugins: [],
 }
